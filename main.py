973014f0641a3b2e899ad7aabd887baeef90d91e
Imsuck910319 <79960711+Imsuck910319@users.noreply.github.com>
2021-03-03 23:30:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics
+e = int(input('輸入數字'))
+m = int(input('輸入數字'))
+i = int(input('輸入數字'))
+l= int(input('輸入數字'))
+t = int(input('輸入數字'))
+r = [e,m,i,l,t]
+print('平均:',myStatistics.myMean(r))
